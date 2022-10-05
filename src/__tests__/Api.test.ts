2b8ef0c6e737f31f9a6935f032bf6346d602ef4f
Roman <iskullbreakeri@gmail.com>
2022-10-05 18:20:46 +0200

    1.0.1

--- src/__tests__/Api.test.ts
+++ src/__tests__/Api.test.ts
@@ -8,6 +8,6 @@ describe('API Tests', () => {
 
   test('Test books list', async () => {
     const books = await instance.getBook({ limit: 1 });
-    expect(books).toEqual([{"_id": "5cf5805fb53e011a64671582", "name": "The Fellowship Of The Ring"}]);
+    expect(books).toEqual([{ _id: '5cf5805fb53e011a64671582', name: 'The Fellowship Of The Ring' }]);
   });
 });
